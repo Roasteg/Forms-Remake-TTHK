6a0a5c487145e7de46990aab53a7d8377f8d66ab
opilane <opilane@HP-CZC6297B8H>
2020-10-13 15:48:18 +0300

    done

--- FormsReborn/Form1.cs
+++ FormsReborn/Form1.cs
@@ -5,11 +5,15 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.IO;
+using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Microsoft.VisualBasic;
 
+using Microsoft.VisualBasic;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Security.Policy;
 
 namespace FormsReborn
 {
@@ -24,6 +28,7 @@ namespace FormsReborn
         PictureBox pic;
         TabControl tabControl;
         MessageBox mbox;
+        ListBox lbox;
         public Form1()
         {
             this.Height = 500;
@@ -43,6 +48,8 @@ namespace FormsReborn
             tn.Nodes.Add(new TreeNode("PildiKast/PictureBox"));
             tn.Nodes.Add(new TreeNode("VaheKaardid/TabControl"));
             tn.Nodes.Add(new TreeNode("MessageBox"));
+            tn.Nodes.Add(new TreeNode("ListBox"));
+            tn.Nodes.Add(new TreeNode("DataGridView"));
             //button
             btn = new Button();
             btn.Text = "Vajuta siia";
@@ -62,6 +69,17 @@ namespace FormsReborn
 
             tree.Nodes.Add(tn);
             this.Controls.Add(tree);
+            //menu
+            MainMenu menu = new MainMenu();
+            MenuItem menuitem1 = new MenuItem("File");
+            MenuItem menuitem2 = new MenuItem("Other");
+            menuitem1.MenuItems.Add("Exit", new EventHandler(menuitem1_Exit));
+            menuitem2.MenuItems.Add("Restart", new EventHandler(menuitem2_restart));
+            menuitem2.MenuItems.Add("About", new EventHandler(menuitem2_about));
+            menuitem2.MenuItems.Add("Help", new EventHandler(menuitem2_help));
+            menu.MenuItems.Add(menuitem1);
+            menu.MenuItems.Add(menuitem2);
+            Menu = menu;
         }
 
 
@@ -137,7 +155,7 @@ namespace FormsReborn
             }
             else if (e.Node.Text == "VaheKaardid/TabControl")
             {
-               var y = MessageBox.Show("Mis vahe?");
+                var y = MessageBox.Show("Mis vahe?");
                 tabControl = new TabControl();
                 tabControl.Location = new Point(300, 400);
                 tabControl.Size = new Size(200, 100);
@@ -151,13 +169,13 @@ namespace FormsReborn
             }
             else if (e.Node.Text == "MessageBox")
             {
-                MessageBox.Show("MessageBox", "Kõige lihtsam aken",);
+                MessageBox.Show("MessageBox", "Kõige lihtsam aken");
                 var answer = MessageBox.Show("Tere", "Aken nupudega", MessageBoxButtons.YesNo);
                 if (answer == DialogResult.Yes)
                 {
                     string text = Interaction.InputBox("Sisesta tekst", "InputBox", "Mingi tekst");
                     var qst = MessageBox.Show("Tere", "Salvesta teks?", MessageBoxButtons.YesNo);
-                    if (qst==DialogResult.Yes)
+                    if (qst == DialogResult.Yes)
                     {
                         lbl.Text = text;
                         this.Controls.Add(lbl);
@@ -167,6 +185,85 @@ namespace FormsReborn
                         MessageBox.Show("Ok");
                     }
                 }
+            }
+            else if (e.Node.Text == "ListBox")
+            {
+                string[] varvid = new string[] { "Kollane","Sinine", "Pruun", "Roheline", "Punane" };
+                var g = varvid.Length;
+                lbox = new ListBox();
+                for (int i = 0; i < g; i++)
+                {
+                    lbox.Items.Add(varvid[i]);
+                }
+                lbox.Location = new Point(300, 40);
+                lbox.Height = g * 15;
+                lbox.Width = 100;
+                lbox.SelectedValueChanged += Lbox_SelectedValueChanged;
+                this.Controls.Add(lbox);
+
+            }
+            else if (e.Node.Text == "DataGridView")
+            {
+                DataSet dataSet = new DataSet("Näide");
+                dataSet.ReadXml("..//..//Files//cdstore.xml");
+                DataGridView dgv = new DataGridView();
+                dgv.Location = new Point(300, 30);
+                dgv.Width = 250;
+                dgv.Height = 250;
+                dgv.AutoGenerateColumns = true;
+                dgv.DataMember = "CD";
+                dgv.DataSource = dataSet;
+                this.Controls.Add(dgv);
+            }
+
+            
+        }
+
+        private void menuitem2_help(object sender, EventArgs e)
+        {
+            MessageBox.Show("Et kasutada, klõpsake pluss ja vali, mida avada", "Kuidas kasutada");
+        }
+
+        private void menuitem2_about(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/Roasteg/FormsReborn");
+        }
+
+        private void Lbox_SelectedValueChanged(object sender, EventArgs e)
+        {
+            var sel = lbox.SelectedItem;
+            if (sel == "Sinine")
+            {
+                lbox.BackColor = Color.Blue;
+            }
+            if (sel == "Roheline")
+            {
+                lbox.BackColor = Color.Green;
+            }
+            if (sel == "Kollane")
+            {
+                lbox.BackColor = Color.Yellow;
+            }
+            if (sel == "Punane")
+            {
+                lbox.BackColor = Color.Red;
+            }
+            if (sel == "Pruun")
+            {
+                lbox.BackColor = Color.Brown;
+            }
+        }
+
+        private void menuitem2_restart(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void menuitem1_Exit(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Kas oled kindel?", "Küsimus", MessageBoxButtons.YesNo)==DialogResult.Yes)
+            {
+                this.Dispose();
             }
         }
 
